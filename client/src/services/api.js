451da97c72021f4600c85af6a17c4bc605a8d11d
Dhyan Nand N <dhyan21008@gmail.com>
2026-06-28 22:50:11 +0530

    Updated API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: " https://bloodconnect-backend-sa84.onrender.com",
 });
 
 export default API;
